c11a189a58c8437f3464b79bd5bef77c5c8169cd
Thorsten Claus <thorstenclaus@web.de>
2014-01-08 21:20:31 +0100

    Built the Information Layout

---
+++ EditorLayout.rb
@@ -0,0 +1,38 @@
+
+
+ # Represents a laout element
+ class EditorLayout
+ 	attr_accessor :attributeAlignment, :attributeName, :controlType, :dataDictionaryID, :dataDictionaryType
+ 	attr_accessor :displayName, :font, :name, :position, :sequence, :showInCardView, :size
+
+
+ 	def htmlStatement
+ 	 # Labels  <label for="attributename">text</label>
+ 	 # Textboxen
+ 	 # checkboxen
+ 	 # Multiline fields
+ 	 # 
+ 	 # Als Div - hovering machen ? 
+ 	 #print "generating HTML output...(#{controlType}) "
+
+ 	 positionAttribute = "style=\"position:absolute; left:#{position['x']}px ;top:#{position['y']}px; height:#{size['height']}px; width:#{size['width']}px; font-weight: normal; font-size: 12px;\""
+
+ 	 if controlType.eql? "LABEL"
+ 	 	label = "<label class=\"text-muted\" for=\"#{attributeName}\" #{positionAttribute} >#{displayName}</label>"
+ 	 	return label
+ 	 end
+
+ 	 if controlType=="TEXT"
+ 	 	inputTag = "<input type=\"text\" id=\"#{attributeName}\"  #{positionAttribute} value=\"{{the value}}\"></input>"
+ 	 	return inputTag
+ 	 end
+ 	 
+ 	 if controlType=="CHECK"
+ 	 	inputTag = "<input type=\"checkbox\" id=\"#{attributeName}\"  #{positionAttribute} ></input>"
+ 	 	return inputTag
+ 	 end
+
+ 	end
+
+
+ end
---
+++ EditorLayouts.rb
@@ -0,0 +1,12 @@
+
+# a list of layout elememnts
+class EditorLayouts
+	attr_accessor :layouts
+	attr_accessor :maxHeight, :maxWidth
+
+	def initialize
+		self.layouts = Array.new #init the array
+	end
+
+
+end
